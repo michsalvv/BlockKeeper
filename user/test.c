093c7d5873b23f16b818c6c23386cdb95b8734ca
michelesalvv <michelesalvv@gmail.com>
2023-07-10 18:00:09 +0200

    [ADD] User interface for VFS read op

--- user/test.c
+++ user/test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 
 #define CLEAR_SCREEN "\033[2J\033[H"
 #define RESET       "\033[0m"
@@ -17,7 +19,7 @@ void action1() {
     int block, ret, size;
     char buffer[4096];
 
-    printf("Enter the block ID: ");
+    printf("%sEnter the block ID: %s", GREEN, RESET);
     scanf("%d", &block);
     printf("Bytes to read: ");
     scanf("%d", &size);
@@ -25,17 +27,18 @@ void action1() {
     printf("Calling GET syscall on block %d\n", block);
     ret = syscall(GET, block, &buffer, size);
     printf("Result: %d\n",ret);
-    
+
     if (ret >0)
         printf("Buffer: \n%s", buffer);
+
+    if (ret == -1 && errno == ENODATA)
+        printf("Returned: %d\n", errno);
 }
 
 void action2() {
-    printf("%sAction 2 selected.%s\n", GREEN, RESET);
+    printf("%sInvalidate block number: %s", GREEN, RESET);
     int value;
-    printf("Enter an integer value: ");
     scanf("%d", &value);
-    printf("You entered: %d\n", value);
 }
 
 void action3() {
@@ -44,6 +47,24 @@ void action3() {
     printf("Enter an integer value: ");
     scanf("%d", &value);
     printf("You entered: %d\n", value);
+}
+
+void vfs_read(){
+    int size, ret;
+    char buffer[4096];
+    int fd = open("mount/the-file", O_RDONLY);
+    if (fd <0){
+        printf("File opening error\n");
+        return;
+    }
+
+    printf("Bytes to read: ");
+    scanf("%d", &size);
+
+    ret = read(fd, buffer, size);
+
+    printf("Readed bytes: %d\n", ret);
+    printf("> %.*s", ret, buffer);
 }
 
 int main() {
@@ -55,7 +76,8 @@ int main() {
         printf("1. Get a Block\n");
         printf("2. Invalidate a Block\n");
         printf("3. Put a Block\n");
-        printf("4. Exit\n");
+        printf("4. Read via VFS\n");
+        printf("5. Exit\n");
 
         printf("\n%sChoose: %s", YELLOW, RESET);
         scanf("%d", &choice);
@@ -71,6 +93,9 @@ int main() {
                 action3();
                 break;
             case 4:
+                vfs_read();
+                break;
+            case 5:
                 printf("%sExiting...%s\n", RED, RESET);
                 break;
             default:
@@ -81,7 +106,7 @@ int main() {
         printf("\nPress Enter to continue...");
         while (getchar() != '\n') {}
         getchar();
-    } while (choice != 4);
+    } while (choice != 5);
 
     return 0;
 }
